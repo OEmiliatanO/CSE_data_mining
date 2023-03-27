73bdf83eb1f0dccc1d49c8ec6897bf279c7abd36
OEmiliatanO <emiliaistruelove@gmail.com>
2023-03-27 20:26:37 +0800

    relocate the include file(data related header files) && use new score function to evaulate the performance

--- hw1/KNN/main.cpp
+++ hw1/KNN/main.cpp
@@ -2,8 +2,9 @@
 #include <memory>
 #include <cmath>
 #include "src/KNN.h"
-#include "src/Dataloader.h"
 #include "src/Brute.h"
+#include "../include/Dataloader.h"
+#include "../include/Datatransformer.h"
 
 int main([[maybe_unused]]int argc, [[maybe_unused]]char* argv[])
 {
@@ -14,24 +15,35 @@ int main([[maybe_unused]]int argc, [[maybe_unused]]char* argv[])
     
     std::cout << "load train data..." << std::endl;
     dataloader.load_train(train_path);
-    std::cerr << "train data =\n" << dataloader.train_data << std::endl;
+    //std::cerr << "train data =\n" << dataloader.train_data << std::endl;
 
     std::cout << "load test data..." << std::endl;
     dataloader.load_test(test_path);
-    std::cerr << "test data =\n" << dataloader.test_data << std::endl;
+    //std::cerr << "test data =\n" << dataloader.test_data << std::endl;
     
     KNN_t KNN{dataloader.train_data};
     KNN.set_knn_method(std::unique_ptr<brute_force>(new brute_force{}));
     std::cout << "predict the test data..." << std::endl;
     dataset_t result = KNN.predict(dataloader.test_data, k);
 
-    int correct = 0;
-    for (std::size_t i = 0; i < result.size(); ++i)
-    {
-        correct += (abs(result[i].label - dataloader.test_data[i].label) < 1e-8);
-    }
+	//auto correct = [](int out, int y) { return (double)(out == y); };
+	auto acc = [&](int out, int y) { return (double)(out == y)*100/result.size(); };
+    //std::cout << "the final accuracy is " << score(result, dataloader.test_data, correct) * 100 / result.size() << '%' << std::endl;
+    std::cout << "the final accuracy is " << score(result, dataloader.test_data, acc) << '%' << std::endl;
+	std::cout << "===================================================================" << std::endl;
+	
+	Dataloader_t dataloader_normal;
+	Datatransformer_t datatransformer;
+	std::cout << "transform the data: normalize the dataset..." << std::endl;
+	dataloader_normal.load_train(datatransformer.normalize(dataloader.train_data));
+	dataloader_normal.load_test(datatransformer.normalize(dataloader.test_data));
+	KNN_t KNN_normal{dataloader_normal.train_data};
+	KNN_normal.set_knn_method(std::unique_ptr<brute_force>(new brute_force{}));
+    std::cout << "predict the test data..." << std::endl;
+    result = KNN_normal.predict(dataloader_normal.test_data, k);
 
-    std::cout << "the final accuracy is " << (double)correct/result.size()*100 << '%' << std::endl;
+    //std::cout << "the final accuracy is " << score(result, dataloader_normal.test_data, correct) * 100 / result.size() << '%' << std::endl;
+    std::cout << "the final accuracy is " << score(result, dataloader_normal.test_data, acc) << '%' << std::endl;
 
     return 0;
 }
