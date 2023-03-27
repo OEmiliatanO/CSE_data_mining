7e1fa27a42a9d21357029df21e5ac4fa56eeebbf
OEmiliatanO <emiliaistruelove@gmail.com>
2023-03-27 20:20:32 +0800

    add KNN_t::train()

--- hw1/KNN/KNN.cpp
+++ hw1/KNN/KNN.cpp
@@ -5,11 +5,20 @@ KNN_t::KNN_t(const dataset_t& dataset)
     this->train_data = dataset;
 }
 
+void KNN_t::train(const dataset_t& dataset)
+{
+    this->train_data = dataset;
+}
+void KNN_t::train(dataset_t&& dataset)
+{
+    this->train_data = dataset;
+}
+
 dataset_t KNN_t::predict(const dataset_t& test_data, size_t k)
 {
-    std::cerr << "KNN_t::predict: calling..." << std::endl;
+    //std::cerr << "KNN_t::predict: calling..." << std::endl;
 	dataset_t result{test_data};
-    std::cerr << "test data = \n" << test_data << std::endl;
+    //std::cerr << "test data = \n" << test_data << std::endl;
 	this->KNN_Method->dataset = this->train_data;
 	for (std::size_t i = 0; i < test_data.size(); ++i)
 	 	result[i].label = this->KNN_Method->predict(test_data[i], k);
