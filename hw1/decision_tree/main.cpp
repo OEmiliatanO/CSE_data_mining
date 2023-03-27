04416a4c4080bdbb66d32e9a6214a9db35234283
OEmiliatanO <emiliaistruelove@gmail.com>
2023-03-27 20:36:37 +0800

    relocate the included files and test normalized test data

--- hw1/decision_tree/main.cpp
+++ hw1/decision_tree/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <memory>
 #include <cmath>
-#include "src/Dataloader.h"
 #include "src/decision_tree.h"
 #include "src/DT_criterion.h"
+#include "../include/Dataloader.h"
+#include "../include/Datatransformer.h"
 
 int main([[maybe_unused]]int argc, [[maybe_unused]]char* argv[])
 {
@@ -32,9 +33,7 @@ int main([[maybe_unused]]int argc, [[maybe_unused]]char* argv[])
     
     int error = 0;
     for (std::size_t i = 0; i < validation.size(); ++i)
-    {
         error += !(abs(validation[i].label - dataloader.train_data[i].label) < minerr);
-    }
     std::cout << "error on train data: " << error << std::endl;
 
     int correct = 0;
@@ -44,7 +43,35 @@ int main([[maybe_unused]]int argc, [[maybe_unused]]char* argv[])
     }
 
     std::cout << "the final accuracy is " << (double)correct/result.size()*100 << '%' << std::endl;
-    std::cout << std::endl;
+
+    std::cout << "=======================================================" << std::endl;
+	std::cout << "transform data: normalize..." << std::endl;
+	Datatransformer_t datatransformer;
+	Dataloader_t dataloader_normal;
+	dataloader_normal.load_train(datatransformer.normalize(dataloader.train_data));
+	//std::cerr << "normalized train data =\n" << dataloader_normal.train_data << std::endl;
+	dataloader_normal.load_test(datatransformer.normalize(dataloader.test_data));
+    tree.build(dataloader_normal.train_data);
+    std::cout << "valid the tree..." << std::endl;
+    validation = tree.predict(dataloader_normal.train_data);
+    
+    error = 0;
+    for (std::size_t i = 0; i < validation.size(); ++i)
+        error += (validation[i].label != dataloader_normal.train_data[i].label);
+    std::cout << "error on train data: " << error << std::endl;
+
+    std::cout << "predict test data..." << std::endl;
+    result = tree.predict(dataloader_normal.test_data);
+    correct = 0;
+    for (std::size_t i = 0; i < result.size(); ++i)
+    {
+        correct += (abs(result[i].label - dataloader_normal.test_data[i].label) < minerr);
+    }
+
+    std::cout << "the final accuracy is " << (double)correct/result.size()*100 << '%' << std::endl;
+
+
+	/*
     std::cout << "set criterion(gini)..." << std::endl;
     tree.set_criterion(std::unique_ptr<DT_criterion>(new gini_crit()));
     std::cout << "build tree..." << std::endl;
@@ -68,7 +95,7 @@ int main([[maybe_unused]]int argc, [[maybe_unused]]char* argv[])
     }
 
     std::cout << "the final accuracy is " << (double)correct/result.size()*100 << '%' << std::endl;
-
+	*/
 
     return 0;
 }
