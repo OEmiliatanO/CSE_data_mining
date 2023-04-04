0756a82c7c77cb3e4bdd844f7bc44005436e0a5f
Abner <abnerlu0214@gmail.com>
2023-04-04 17:57:00 +0800

    照老大說的做ㄌ🦈🦈🦈

--- hw1/PLA_pocket/pla_pocket.py
+++ hw1/PLA_pocket/pla_pocket.py
@@ -1,49 +1,33 @@
 import pandas as pd
-from sklearn.linear_model import Perceptron
-import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.linear_model import Perceptron
 
+# input the data
+AorB = input('Select your data (A or B ?) : ')
+if (AorB == 'A' or 'a'):
+    train_data = pd.read_csv('../data/testA/train_data.csv')
+    test_data = pd.read_csv('../data/testA/test_data.csv')
+else:
+    train_data = pd.read_csv('../data/testB/train_data.csv')
+    test_data = pd.read_csv('../data/testB/test_data.csv')
 
-train_data = pd.read_csv('train_data.csv')
-test_data = pd.read_csv('test_data.csv')
+num_test = input('Number of tests: ')
+num_test = int(num_test)
+i = 0
+sum_accuracy = 0
+while i < num_test:
 
-# 從資料中分離出特徵和標籤
-X_train = train_data.iloc[:, :-1]
-y_train = train_data.iloc[:, -1]
-X_test = test_data.iloc[:, :-1]
-y_test = test_data.iloc[:, -1]
+    X_train = train_data.iloc[:,:-1].values
+    X_test = test_data.iloc[:,:-1].values
+    y_train = train_data.iloc[:,-1].values
+    y_test = test_data.iloc[:,-1].values
 
-# 建立 Perceptron 模型
-clf = Perceptron()
+    clf = Perceptron()
+    # train model
+    clf.fit(X_train, y_train)
+    # calculate accuracy
+    accuracy = clf.score(X_test, y_test)
+    sum_accuracy += accuracy
+    i += 1
 
-# 訓練模型
-clf.fit(X_train, y_train)
-
-# 評估模型
-accuracy = clf.score(X_test, y_test)
-print("Accuracy:", accuracy)
-
-# plot hyperplane
-w = clf.coef_[0]
-b = clf.intercept_[0]
-X_min = X_train.min() - 1
-X_max = X_train.max() + 1
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(X_train.iloc[:, 0], X_train.iloc[:, 1], X_train.iloc[:, 2], c=y_train)
-
-# 網格
-xx, yy = np.meshgrid(np.arange(X_min[0], X_max[0], 0.2), 
-                     np.arange(X_min[1], X_max[1], 0.2))
-zz = (-w[0]*xx - w[1]*yy - b) / w[2]
-
-# hyperplane
-ax.plot_surface(xx, yy, zz, alpha=0.6)
-
-# xyz axis
-ax.set_xlabel('X1')
-ax.set_ylabel('X2')
-ax.set_zlabel('X3')
-
-plt.show()
+print(f"Using data {AorB}. {num_test} times average accuracy: {sum_accuracy / num_test:.5f}")
