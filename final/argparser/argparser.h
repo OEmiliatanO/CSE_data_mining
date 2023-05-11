e83c37b8641624fbd1402da36fed3335101358b6
OEmiliatanO <emiliaistruelove@gmail.com>
2023-05-11 15:37:32 +0800

    update argparser

---
+++ final/argparser/argparser.h
@@ -0,0 +1,33 @@
+#include <vector>
+#include <unordered_map>
+#include <string>
+
+class argparser
+{
+	argparser() = default;
+	
+	std::vector<std::string> to_parse;
+	std::unordered_map<std::string, std::string> args;
+
+	void parse(std::size_t argc, char **argv);
+	void add(std::string arg);
+};
+
+void add(std::string arg)
+{
+	to_parse.emplace_back(arg);
+}
+
+void argparser::parse(std::size_t argc, char **argv)
+{
+	for (std::size_t i = 1; i < argc; ++i)
+	{
+		std::string arg{argv[i]};
+		for (std::size_t j = 0; j < to_parse.size(); ++j)
+			if (arg == to_parse[j])
+			{
+				args[arg] = argv[++i];
+				break;
+			}
+	}
+}
