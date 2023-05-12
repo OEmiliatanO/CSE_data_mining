ec05a2d17293cbfe86371dccc4fbe6478e0f8844
OEmiliatanO <emiliaistruelove@gmail.com>
2023-05-12 11:54:49 +0800

    update transformer

--- final/data/gene_expression_cancer_RNA-Seq_data_set/label2int.py
+++ final/data/gene_expression_cancer_RNA-Seq_data_set/label2int.py
@@ -1,3 +1,12 @@
+import sys
+
 KNOWN_LABEL = ["KIRC", "BRCA", "LUAD"]
 UNKNOWN_LABEL = ["COAD", "PRAD"]
 
+label_map = {"KIRC" : 1, "BRCA": 2, "LUAD": 3, "COAD": 4, "PRAD": 5}
+
+try:
+    while True:
+        print(label_map[input().strip()])
+except:
+    pass
