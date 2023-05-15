96f14e0d2478c2cd16d4f62bb615dde79ab2e5fb
OEmiliatanO <emiliaistruelove@gmail.com>
2023-05-15 09:11:04 +0800

    update script

--- final/preprocess/Autoencoder/run.sh
+++ final/preprocess/Autoencoder/run.sh
@@ -1,3 +1,6 @@
-python main.py 279 32 ../../data/Arrhythmia_data_set/train_data.csv ../../data/Arrhythmia_data_set/train_label.csv ../../data/Arrhythmia_data_set/test_data.csv ../../data/Arrhythmia_data_set/test_label.csv ../../data/Arrhythmia_data_set/AE_train_data.csv ../../data/Arrhythmia_data_set/AE_test_data.csv loss.png
-
-#python main.py 20531 30  ../../data/gene_expression_cancer_RNA-Seq_data_set/train_data.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/train_label.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/test_data.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/test_label.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/AE_train_data.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/AE_test_data.csv loss.png
+Aout=32
+python main.py 279 $Aout ../../data/Arrhythmia_data_set/train_data.csv ../../data/Arrhythmia_data_set/train_label.csv ../../data/Arrhythmia_data_set/test_data.csv ../../data/Arrhythmia_data_set/test_label.csv ../../data/Arrhythmia_data_set/AE_train_data_$Aout.csv ../../data/Arrhythmia_data_set/AE_test_data_$Aout.csv loss.png
+Aout=24
+python main.py 279 $Aout ../../data/Arrhythmia_data_set/train_data.csv ../../data/Arrhythmia_data_set/train_label.csv ../../data/Arrhythmia_data_set/test_data.csv ../../data/Arrhythmia_data_set/test_label.csv ../../data/Arrhythmia_data_set/AE_train_data_$Aout.csv ../../data/Arrhythmia_data_set/AE_test_data_$Aout.csv loss.png
+#Bout=30
+#python main.py 20531 30  ../../data/gene_expression_cancer_RNA-Seq_data_set/train_data.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/train_label.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/test_data.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/test_label.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/AE_train_data_$Bout.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/AE_test_data_$Bout.csv loss.png
--- final/preprocess/PCA/run.sh
+++ final/preprocess/PCA/run.sh
@@ -1,3 +1,6 @@
-python main.py 32 ../../data/Arrhythmia_data_set/train_data.csv ../../data/Arrhythmia_data_set/train_label.csv ../../data/Arrhythmia_data_set/test_data.csv ../../data/Arrhythmia_data_set/test_label.csv ../../data/Arrhythmia_data_set/PCA_train_data.csv ../../data/Arrhythmia_data_set/PCA_test_data.csv
-
-python main.py 100 ../../data/gene_expression_cancer_RNA-Seq_data_set/train_data.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/train_label.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/test_data.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/test_label.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/PCA_train_data.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/PCA_test_data.csv
+Aout=32
+python main.py $Aout ../../data/Arrhythmia_data_set/train_data.csv ../../data/Arrhythmia_data_set/train_label.csv ../../data/Arrhythmia_data_set/test_data.csv ../../data/Arrhythmia_data_set/test_label.csv ../../data/Arrhythmia_data_set/PCA_train_data_$Aout.csv ../../data/Arrhythmia_data_set/PCA_test_data_$Aout.csv
+Aout=31
+python main.py $Aout ../../data/Arrhythmia_data_set/train_data.csv ../../data/Arrhythmia_data_set/train_label.csv ../../data/Arrhythmia_data_set/test_data.csv ../../data/Arrhythmia_data_set/test_label.csv ../../data/Arrhythmia_data_set/PCA_train_data_$Aout.csv ../../data/Arrhythmia_data_set/PCA_test_data_$Aout.csv
+#Bout=100
+#python main.py 100 ../../data/gene_expression_cancer_RNA-Seq_data_set/train_data.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/train_label.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/test_data.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/test_label.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/PCA_train_data_$Bout.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/PCA_test_data_$Bout.csv
