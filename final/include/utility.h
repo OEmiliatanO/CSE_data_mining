5f67b58fb3bac12a17b8f7dbdaf3b1467d6fab92
OEmiliatanO <emiliaistruelove@gmail.com>
2023-05-18 19:29:30 +0800

    bug fix

--- final/include/utility.h
+++ final/include/utility.h
@@ -7,7 +7,7 @@
 template<typename T, typename U>
 std::size_t assign(const U& x, const std::vector<T>& centers)
 {
-    std::pair<double, std::size_t> res{std::numeric_limits<double>::lowest(), 0};
+    std::pair<double, std::size_t> res{std::numeric_limits<double>::max(), 0};
     for (std::size_t i = 0; i < centers.size(); ++i)
         res = std::min(res, std::pair{euclidean_dist(centers[i], x), i});
     return res.second;
