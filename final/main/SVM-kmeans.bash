f4f581192f0d08c3296ae47ffcfe056737a23da3
OEmiliatanO <emiliaistruelove@gmail.com>
2023-05-19 00:35:05 +0800

    update test script

--- final/main/SVM-kmeans.bash
+++ final/main/SVM-kmeans.bash
@@ -2,11 +2,11 @@
 DATASET1="../data/Arrhythmia_data_set"
 DATASET2="../data/gene_expression_cancer_RNA-Seq_data_set"
 
-./main.elf -repeats 10 -classifying SVM -clustering kmeans -train_data_path $DATASET1/train_data.csv -train_label_path $DATASET1/train_label.csv -test_data_path $DATASET1/test_data.csv -test_label_path $DATASET1/test_label.csv -normalize false -KNOWN_CNT 8 -UNKNOWN_CNT 5 -SVM_converge_lim 1e-7 -SVM_punishment 3 -kmeans_converge_lim 1e-7
+./main.elf -repeats 10 -classifying SVM -clustering kmeans -train_data_path $DATASET1/train_data.csv -train_label_path $DATASET1/train_label.csv -test_data_path $DATASET1/test_data.csv -test_label_path $DATASET1/test_label.csv -normalize false -KNOWN_CNT 8 -UNKNOWN_CNT 5 -SVM_converge_lim 1e-7 -SVM_punishment 0.1 -kmeans_converge_lim 1e-7
 
-./main.elf -repeats 10 -classifying SVM -clustering kmeans -train_data_path $DATASET1/AE_train_data_32.csv -train_label_path $DATASET1/train_label.csv -test_data_path $DATASET1/AE_test_data_32.csv -test_label_path $DATASET1/test_label.csv -normalize false -KNOWN_CNT 8 -UNKNOWN_CNT 5 -SVM_converge_lim 1e-7 -SVM_punishment 3 -kmeans_converge_lim 1e-7
+./main.elf -repeats 10 -classifying SVM -clustering kmeans -train_data_path $DATASET1/AE_train_data_32.csv -train_label_path $DATASET1/train_label.csv -test_data_path $DATASET1/AE_test_data_32.csv -test_label_path $DATASET1/test_label.csv -normalize false -KNOWN_CNT 8 -UNKNOWN_CNT 5 -SVM_converge_lim 1e-7 -SVM_punishment 0.1 -kmeans_converge_lim 1e-7
 
-./main.elf -repeats 10 -classifying SVM -clustering kmeans -train_data_path $DATASET1/PCA_train_data_32.csv -train_label_path $DATASET1/train_label.csv -test_data_path $DATASET1/PCA_test_data_32.csv -test_label_path $DATASET1/test_label.csv -normalize false -KNOWN_CNT 8 -UNKNOWN_CNT 5 -SVM_converge_lim 1e-7 -SVM_punishment 3 -kmeans_converge_lim 1e-7
+./main.elf -repeats 10 -classifying SVM -clustering kmeans -train_data_path $DATASET1/PCA_train_data_32.csv -train_label_path $DATASET1/train_label.csv -test_data_path $DATASET1/PCA_test_data_32.csv -test_label_path $DATASET1/test_label.csv -normalize false -KNOWN_CNT 8 -UNKNOWN_CNT 5 -SVM_converge_lim 1e-7 -SVM_punishment 0.1 -kmeans_converge_lim 1e-7
 
 echo "========"
 
