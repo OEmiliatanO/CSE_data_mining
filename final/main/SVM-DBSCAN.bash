f4155bbd39e9488b5980bdcfc8365d9257eca9b6
OEmiliatanO <emiliaistruelove@gmail.com>
2023-05-19 17:30:28 +0800

    update the parameter

--- final/main/SVM-DBSCAN.bash
+++ final/main/SVM-DBSCAN.bash
@@ -2,18 +2,18 @@
 DATASET1="../data/Arrhythmia_data_set"
 DATASET2="../data/gene_expression_cancer_RNA-Seq_data_set"
 
-./main.elf -repeats 1 -classifying SVM -clustering DBSCAN -train_data_path $DATASET1/train_data.csv -train_label_path $DATASET1/train_label.csv -test_data_path $DATASET1/test_data.csv -test_label_path $DATASET1/test_label.csv -normalize false -KNOWN_CNT 8 -UNKNOWN_CNT 5 -SVM_converge_lim 1e-7 -SVM_punishment 1 -DBSCAN_minPts 10 -DBSCAN_eps 500
+echo "dataset: arrhythmia"
 
-./main.elf -repeats 1 -classifying SVM -clustering DBSCAN -train_data_path $DATASET1/AE_train_data_32.csv -train_label_path $DATASET1/train_label.csv -test_data_path $DATASET1/AE_test_data_32.csv -test_label_path $DATASET1/test_label.csv -normalize false -KNOWN_CNT 8 -UNKNOWN_CNT 5 -SVM_converge_lim 1e-7 -SVM_punishment 1 -DBSCAN_minPts 10 -DBSCAN_eps 500
+#./main.elf -repeats 10 -classifying SVM -clustering DBSCAN -train_data_path $DATASET1/train_data.csv -train_label_path $DATASET1/train_label.csv -test_data_path $DATASET1/test_data.csv -test_label_path $DATASET1/test_label.csv -normalize false -KNOWN_CNT 8 -UNKNOWN_CNT 5 -SVM_converge_lim 1e-7 -SVM_punishment 1 -SVM_kernel linear -SVM_kernel_gamma 2 -SVM_kernel_degree 2 -SVM_kernel_r 1 -DBSCAN_minPts 10 -DBSCAN_eps 500
 
-./main.elf -repeats 1 -classifying SVM -clustering DBSCAN -train_data_path $DATASET1/PCA_train_data_32.csv -train_label_path $DATASET1/train_label.csv -test_data_path $DATASET1/PCA_test_data_32.csv -test_label_path $DATASET1/test_label.csv -normalize false -KNOWN_CNT 8 -UNKNOWN_CNT 5 -SVM_converge_lim 1e-7 -SVM_punishment 1 -DBSCAN_minPts 10 -DBSCAN_eps 500
+#./main.elf -repeats 10 -classifying SVM -clustering DBSCAN -train_data_path $DATASET1/AE_train_data_32.csv -train_label_path $DATASET1/train_label.csv -test_data_path $DATASET1/AE_test_data_32.csv -test_label_path $DATASET1/test_label.csv -normalize false -KNOWN_CNT 8 -UNKNOWN_CNT 5 -SVM_converge_lim 1e-7 -SVM_punishment 1 -SVM_kernel linear -SVM_kernel_gamma 2 -SVM_kernel_degree 2 -SVM_kernel_r 1 -DBSCAN_minPts 10 -DBSCAN_eps 500
 
-echo "========"
+#./main.elf -repeats 10 -classifying SVM -clustering DBSCAN -train_data_path $DATASET1/PCA_train_data_32.csv -train_label_path $DATASET1/train_label.csv -test_data_path $DATASET1/PCA_test_data_32.csv -test_label_path $DATASET1/test_label.csv -normalize false -KNOWN_CNT 8 -UNKNOWN_CNT 5 -SVM_converge_lim 1e-7 -SVM_punishment 1 -SVM_kernel linear -SVM_kernel_gamma 2 -SVM_kernel_degree 2 -SVM_kernel_r 1 -DBSCAN_minPts 10 -DBSCAN_eps 500
 
-#./main.elf -repeats 1 -classifying SVM -clustering DBSCAN -train_data_path $DATASET2/train_data.csv -train_label_path $DATASET2/train_label.csv -test_data_path $DATASET2/test_data.csv -test_label_path $DATASET2/test_label.csv -normalize false -KNOWN_CNT 3 -UNKNOWN_CNT 2 -SVM_converge_lim 1e-7 -SVM_punishment 1 -DBSCAN_minPts 22 -DBSCAN_eps 80
+echo "dataset: gene expression cancer"
 
-#./main.elf -repeats 1 -classifying SVM -clustering DBSCAN -train_data_path $DATASET2/AE_train_data_30.csv -train_label_path $DATASET2/train_label.csv -test_data_path $DATASET2/AE_test_data_30.csv -test_label_path $DATASET2/test_label.csv -normalize false -KNOWN_CNT 3 -UNKNOWN_CNT 2 -SVM_converge_lim 1e-7 -SVM_punishment 1 -DBSCAN_minPts 22 -DBSCAN_eps 80
+#./main.elf -repeats 10 -classifying SVM -clustering DBSCAN -train_data_path $DATASET2/train_data.csv -train_label_path $DATASET2/train_label.csv -test_data_path $DATASET2/test_data.csv -test_label_path $DATASET2/test_label.csv -normalize false -KNOWN_CNT 3 -UNKNOWN_CNT 2 -SVM_converge_lim 1e-7 -SVM_punishment 1 -SVM_kernel rbf -SVM_kernel_gamma 2 -SVM_kernel_degree 2 -SVM_kernel_r 1 -DBSCAN_minPts 22 -DBSCAN_eps 80
 
-#./main.elf -repeats 1 -classifying SVM -clustering DBSCAN -train_data_path $DATASET2/PCA_train_data_30.csv -train_label_path $DATASET2/train_label.csv -test_data_path $DATASET2/PCA_test_data_30.csv -test_label_path $DATASET2/test_label.csv -normalize false -KNOWN_CNT 3 -UNKNOWN_CNT 2 -SVM_converge_lim 1e-7 -SVM_punishment 1 -DBSCAN_minPts 10 -DBSCAN_eps 460 -kmeans_converge_lim 1e-7
+./main.elf -repeats 10 -classifying SVM -clustering DBSCAN -train_data_path $DATASET2/AE_train_data_30.csv -train_label_path $DATASET2/train_label.csv -test_data_path $DATASET2/AE_test_data_30.csv -test_label_path $DATASET2/test_label.csv -normalize false -KNOWN_CNT 3 -UNKNOWN_CNT 2 -SVM_converge_lim 1e-7 -SVM_punishment 0.5 -SVM_kernel poly -SVM_kernel_gamma 1 -SVM_kernel_degree 2 -SVM_kernel_r 1 -DBSCAN_minPts 22 -DBSCAN_eps 50
 
-echo "========"
+./main.elf -repeats 10 -classifying SVM -clustering DBSCAN -train_data_path $DATASET2/PCA_train_data_30.csv -train_label_path $DATASET2/train_label.csv -test_data_path $DATASET2/PCA_test_data_30.csv -test_label_path $DATASET2/test_label.csv -normalize false -KNOWN_CNT 3 -UNKNOWN_CNT 2 -SVM_converge_lim 1e-7 -SVM_punishment 1 -SVM_kernel poly -SVM_kernel_gamma 2 -SVM_kernel_degree 2 -SVM_kernel_r 1 -DBSCAN_minPts 10 -DBSCAN_eps 460 -kmeans_converge_lim 1e-7
