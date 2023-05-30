701012d23a108e3fa360e580b817b321808dfaf0
Emiliatan <47825105+OEmiliatanO@users.noreply.github.com>
2023-05-30 22:46:06 +0800

    Update run.sh

--- final/preprocess/PCA/run.sh
+++ final/preprocess/PCA/run.sh
@@ -1,8 +1,4 @@
-#Aout=32
-#python main.py $Aout ../../data/Arrhythmia_data_set/train_data.csv ../../data/Arrhythmia_data_set/train_label.csv ../../data/Arrhythmia_data_set/test_data.csv ../../data/Arrhythmia_data_set/test_label.csv ../../data/Arrhythmia_data_set/PCA_train_data_$Aout.csv ../../data/Arrhythmia_data_set/PCA_test_data_$Aout.csv
-#Aout=31
-#python main.py $Aout ../../data/Arrhythmia_data_set/train_data.csv ../../data/Arrhythmia_data_set/train_label.csv ../../data/Arrhythmia_data_set/test_data.csv ../../data/Arrhythmia_data_set/test_label.csv ../../data/Arrhythmia_data_set/PCA_train_data_$Aout.csv ../../data/Arrhythmia_data_set/PCA_test_data_$Aout.csv
+Aout=32
+python main.py $Aout ../../data/Arrhythmia_data_set/train_data.csv ../../data/Arrhythmia_data_set/train_label.csv ../../data/Arrhythmia_data_set/test_data.csv ../../data/Arrhythmia_data_set/test_label.csv ../../data/Arrhythmia_data_set/PCA_train_data_$Aout.csv ../../data/Arrhythmia_data_set/PCA_test_data_$Aout.csv
 Bout=30
-python main.py 30 ../../data/gene_expression_cancer_RNA-Seq_data_set/train_data.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/train_label.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/test_data.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/test_label.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/PCA_train_data_$Bout.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/PCA_test_data_$Bout.csv
-Bout=100
-python main.py 100 ../../data/gene_expression_cancer_RNA-Seq_data_set/train_data.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/train_label.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/test_data.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/test_label.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/PCA_train_data_$Bout.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/PCA_test_data_$Bout.csv
+python main.py $B ../../data/gene_expression_cancer_RNA-Seq_data_set/train_data.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/train_label.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/test_data.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/test_label.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/PCA_train_data_$Bout.csv ../../data/gene_expression_cancer_RNA-Seq_data_set/PCA_test_data_$Bout.csv
